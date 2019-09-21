bb70a20cf93f000514aa4ac9b7ecfc8238fa9989
victorhcortesp <vcortes@condorlabs.io>
2019-09-21 08:49:59 -0500

    intro svelte

---
+++ src/DogItem.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let id;
+  export let name;
+  export let image;
+</script>
+
+<style>
+  article {
+    cursor: pointer;
+  }
+</style>
+
+<article on:click={() => dispatch('selectitem', { id })}>
+  <h1>{name}</h1>
+  <img alt={name} src={image} />
+</article>
---
+++ src/Modal.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  const closeModal = () => dispatch("closemodal");
+</script>
+
+<style>
+  .modal {
+    text-align: center;
+    /* position: relative;  */
+    margin: 7% auto;
+    padding: 30px 50px;
+    background-color: #fafafa;
+    color: #333;
+    border-radius: 3px;
+    width: 50%;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 2em;
+  }
+</style>
+
+<div class="back" on:click={closeModal} />
+<div class="modal" id="openModal">
+  <div>
+    <header>
+      <slot name="header" />
+    </header>
+    <div class="content">
+      <slot />
+    </div>
+    <footer>
+      <slot name="footer">
+        <button on:click={closeModal}>Close</button>
+      </slot>
+    </footer>
+  </div>
+</div>
